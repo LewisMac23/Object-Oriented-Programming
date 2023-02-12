31c1886fc8b05f68da8f6d191a01d11ef9088f89
LewisMac23 <124689702+LewisMac23@users.noreply.github.com>
2023-02-12 21:15:00 +0000

    Add files via upload

---
+++ PolymorphismCompileVsRunTime/PolymorphismCompileVsRunTime/PolymorphismCompileVsRunTime.cpp
@@ -0,0 +1,15 @@
+// PolymorphismCompileVsRunTime.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+//Theory in yt vid simplesnippets
+
+int main()
+{
+
+	
+
+	return 0;
+}
+
